e908260f32a0cc69250dd1726b54bc28aa683dfb
C-Pflugfelder <c-pflugfeldereb.de>
2019-06-19 15:56:51 +0200

    basics

---
+++ BatteryIndicator/Form1.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace BatteryIndicator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(0, 0);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Location = new System.Drawing.Point(-9999, -9999);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ BatteryIndicator/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace BatteryIndicator
+{
+    public partial class Form1 : Form
+    {
+        private readonly NotifyIcon notifyIcon = new NotifyIcon();
+        private float LastPercentage;
+        public Form1()
+        {
+            InitializeComponent();
+
+            float BatteryPercentage = SystemInformation.PowerStatus.BatteryLifePercent * 100;
+            CreateIcon(BatteryPercentage);
+            LastPercentage = BatteryPercentage;
+
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 10000; // 10 seconds
+            timer.Elapsed += new ElapsedEventHandler(OnTimer);
+            timer.Start();
+
+            notifyIcon.Visible = true;
+        }
+
+        private void OnTimer(object sender, ElapsedEventArgs e)
+        {
+            float BatteryPercentage = SystemInformation.PowerStatus.BatteryLifePercent * 100;
+
+            if (LastPercentage != BatteryPercentage)
+            {
+                CreateIcon(BatteryPercentage);
+                LastPercentage = BatteryPercentage;
+            }
+        }
+
+        private void CreateIcon(float BatteryPercentage)
+        {
+            try
+            {
+                //Load the Image to be written on
+                Bitmap bitMapImage = new Bitmap(50, 50);
+                Graphics graphicImage = Graphics.FromImage(bitMapImage);
+
+                //Write text
+                graphicImage.DrawString(BatteryPercentage.ToString(), new Font("Arial", 25), new SolidBrush(System.Drawing.Color.White), new System.Drawing.Point(-5, 3));
+
+                Icon icon = System.Drawing.Icon.FromHandle(bitMapImage.GetHicon());
+                notifyIcon.Icon = icon;
+
+                //Clean house
+                graphicImage.Dispose();
+                bitMapImage.Dispose();
+
+                //Change Tooltip
+                int timeLeft = (int)SystemInformation.PowerStatus.BatteryLifeRemaining / 60;
+                int hrLeft = timeLeft / 60;
+                int minLeft = timeLeft % 60;
+                if (hrLeft != 0)
+                {
+                    notifyIcon.Text = $"{hrLeft} hr";
+                }
+                notifyIcon.Text += $"{minLeft} min";
+            }
+            catch
+            {
+            }
+        }
+    }
+}
